dbed6192c0a1fe966c4f1100e153c08033d48b6a
Viachaslau Kalevich <cto@noirix.com>
2020-10-20 21:10:47 +0300

    Spring Start 2: add properties reading

--- src/main/java/com/noirix/SpringContextTester.java
+++ src/main/java/com/noirix/SpringContextTester.java
@@ -1,6 +1,8 @@
 package com.noirix;
 
 import com.noirix.domain.User;
+import com.noirix.util.DatabaseConfig;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class SpringContextTester {
@@ -8,9 +10,25 @@ public class SpringContextTester {
         ClassPathXmlApplicationContext classPathXmlApplicationContext = new ClassPathXmlApplicationContext("classpath:application-context.xml");
 
         User user1 = (User)classPathXmlApplicationContext.getBean("user1");
+        User user2 = (User)classPathXmlApplicationContext.getBean("user2");
 
         System.out.println(user1.getId());
         System.out.println(user1.getName());
         System.out.println(user1.getSurname());
+
+        System.out.println(user2.getId());
+        System.out.println(user2.getName());
+        System.out.println(user2.getSurname());
+
+
+        System.out.println("******************************************");
+
+        AnnotationConfigApplicationContext annotationConfigApplicationContext = new AnnotationConfigApplicationContext("com.noirix");
+
+        DatabaseConfig bean = annotationConfigApplicationContext.getBean(DatabaseConfig.class);
+        System.out.println(bean.getLogin());
+        System.out.println(bean.getDriverName());
+        System.out.println(bean.getPassword());
+        System.out.println(bean.getUrl());
     }
 }
